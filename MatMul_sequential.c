6a3d9f33b895357d48a36c3c4ebf13898254b6b3
supasitk <supasit.kajkamhaeng@gmail.com>
2018-01-26 17:05:52 +0700

    commit

--- MatMul_sequential.c
+++ MatMul_sequential.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
+void usage(void)
 {
-	printf("hello\n");
+	printf("Usage:\n");
+	printf("\t-s <size>\n");
+	exit(-1);
+}
+
+int main(int argc, char *argv[])
+{
+	int size;
+
+	while ((argc > 1) && (argv[1][0] == '-'))
+	{
+		switch (argv[1][1])
+		{
+			case 's':
+				size = atoi(argv[2]);
+				break;
+
+			default:
+				printf("Wrong argument: %s\n", argv[1]);
+				usage();
+		}
+		argv += 2;
+		argc -= 2;
+	}	
+
+	printf("Matrix size: %d\n", size);
 
 	return 0;
 }
