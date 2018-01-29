ca8a628a609f06c75ef56249472f05a2aeef6711
supasitk <supasit.kajkamhaeng@gmail.com>
2018-01-29 17:45:24 +0700

    commit

--- MatMul_sequential.c
+++ MatMul_sequential.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+#define HIGH 	10
+#define LOW 	1
 
 void usage(void)
 {
@@ -10,8 +14,13 @@ void usage(void)
 
 int main(int argc, char *argv[])
 {
+	int i, j, k, sum;
+	time_t seed, start, end;
+	double cpu_time_used;
 	int size;
+	int **A, **B, **C;
 
+	/* Extract parameters of command */
 	while ((argc > 1) && (argv[1][0] == '-'))
 	{
 		switch (argv[1][1])
@@ -28,7 +37,61 @@ int main(int argc, char *argv[])
 		argc -= 2;
 	}	
 
-	printf("Matrix size: %d\n", size);
+	/* Allocate memory and initialize value of matrix */ 	
+	printf("Initializing Matrix [%dx%d]........\n", size, size);
+	start = clock();
+	
+	A = malloc(size * sizeof(int *));
+	B = malloc(size * sizeof(int *));
+	C = malloc(size * sizeof(int *));
+	for (i=0; i<size; i++)
+	{
+		A[i] = malloc(size * sizeof(int));
+		B[i] = malloc(size * sizeof(int));
+		C[i] = malloc(size * sizeof(int));
+	} 
+
+	time(&seed);
+	srand((unsigned int) seed);
+	for (i=0; i<size; i++)
+	{
+		for (j=0; j<size; j++)
+		{
+			A[i][j] = rand() % (HIGH - LOW + 1) + LOW;
+			B[i][j] = rand() % (HIGH - LOW + 1) + LOW;
+		}
+	}
+
+	end = clock();
+	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+	printf("Execution Time = %f seconds.\n", cpu_time_used);
+	printf("----------------------------------------------------------------\n");
+
+
+	/* Matrix multiplication */
+	printf("Matrix Multiplication is being processed........\n");
+	start = clock();
+	
+	sum = 0;
+	for (i=0; i<size; i++)
+	{
+		for (j=0; j<size; j++)
+		{
+			for (k=0; k<size; k++)
+			{
+				sum = sum + (A[i][k] * B[k][j]); 
+			}
+		}
+		C[i][j] = sum;
+		sum = 0;
+	}
+
+	end = clock();
+	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+	printf("Execution Time = %f seconds.\n", cpu_time_used);
+	printf("----------------------------------------------------------------\n");
+	
+	printf("Completed!!!\n");
 
 	return 0;
 }
